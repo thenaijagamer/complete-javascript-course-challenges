37c344b99105c7471c83a85097ae6a867546260e
thenaijagamer <sabdullahialaba50@gmail.com>
2023-12-26 22:07:10 +0100

    Complete the first challenge of the Data structures section

--- 05 data Structures, modern operators and strings/challenge #1.js
+++ 05 data Structures, modern operators and strings/challenge #1.js
@@ -63,8 +63,32 @@ const game = {
   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
   date: "Nov 9th, 2037",
   odds: {
-    team1: 1.33,
+    team1: 11.33,
     x: 3.25,
     team2: 6.5,
   },
 };
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(allPlayers, players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  for (let i = 0; i < players.length; i++) console.log(players[i]);
+  console.log(`Goals scored: ${players.length}`);
+};
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
+
+// Team that is more likely to win
+team1 > team2 || console.log(`Team 1 is more likely to win`);
+team2 > team1 || console.log(`Team 2 is more likely to win`);
