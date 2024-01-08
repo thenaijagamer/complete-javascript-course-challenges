a96f053a9e1c1851d5e245082f1eeb6994ca96aa
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-08 17:10:49 +0100

    Complete challenge 1

--- 07 working with arrays/challenge #1.js
+++ 07 working with arrays/challenge #1.js
@@ -23,4 +23,25 @@ Test data:
 Hints: Use tools from all lectures in this section so far �
  */
 
-const checkDogs = function (dogsJulia, dogsKate) {};
+const checkDogs = function (dogsJulia, dogsKate) {
+  // 1
+  // filter cats out
+  const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+
+  // 2
+  const allDogs = dogsJuliaCorrected.concat(dogsKate);
+
+  // 3
+  allDogs.forEach((dogAge, i) => {
+    const message =
+      dogAge < 3
+        ? `Dog number ${i + 1} is still a puppy `
+        : `Dog number ${i + 1} is an adult, and is ${dogAge} years old`;
+    console.log(message);
+  });
+};
+
+// 4
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log("_________________________");
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
