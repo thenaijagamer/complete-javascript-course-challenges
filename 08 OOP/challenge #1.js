3d0d74af53c6c396528e50a76a4604b8b04a59c3
thenaijagamer <sabdullahialaba50@gmail.com>
2024-02-02 00:30:42 +0100

    Start challenge 3 of OOP

--- 08 OOP/challenge #1.js
+++ 08 OOP/challenge #1.js
@@ -23,6 +23,7 @@ Car.prototype.accelerate = function () {
   this.speed += 10;
   console.log(this.make + " going at " + this.speed + "km/h");
 };
+
 Car.prototype.brake = function () {
   this.speed -= 5;
   console.log(this.make + " going at " + this.speed + "km/h");
