cfeeba7ee2117c98189a9b664faaa62696aad5e9
thenaijagamer <sabdullahialaba50@gmail.com>
2024-02-05 18:44:08 +0100

    Complete challenge 4 of OOP

--- 08 OOP/challenge #4.js
+++ 08 OOP/challenge #4.js
@@ -10,3 +10,95 @@ Test data:
 § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
 GOOD LUCK �
  */
+
+// 1
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(this.make + " going at " + this.speed + "km/h");
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(this.make + " going at " + this.speed + "km/h");
+  }
+
+  get speedUS() {
+    console.log(this.make + " going at " + this.speed / 1.6 + "mi/h");
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+// 1
+class EVCl extends CarCl {
+  // 2
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    this.#charge -= 1;
+    console.log(
+      "Accelerate:",
+      this.make +
+        " going at " +
+        this.speed +
+        "km/h," +
+        " with a charge of " +
+        this.#charge
+    );
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log("Brake:", this.make + " now going at " + this.speed + "km/h");
+    return this;
+  }
+}
+
+const electCar = new EVCl("Rivian", 120, 23);
+
+// 3
+electCar
+  .accelerate()
+  .chargeBattery(80)
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .brake()
+  .brake()
+  .brake()
+  .brake()
+  .brake()
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .brake()
+  .accelerate()
+  .brake()
+  .accelerate()
+  .brake()
+  .accelerate();
